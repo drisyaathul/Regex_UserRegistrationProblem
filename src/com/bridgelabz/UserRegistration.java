2ce38b1bc2930cfe9feabdbb29e10945bc337241
drisyaathul <drisyak809@gmail.com>
2023-05-06 16:06:13 +0800

    Added: Enter Valid Last Name

--- src/com/bridgelabz/UserRegistration.java
+++ src/com/bridgelabz/UserRegistration.java
@@ -1,21 +1,27 @@
 package com.bridgelabz;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("*** Welcome to User Registration Problem ***");
-        /*
-         UC1: User need to enter Valid firstname
-         */
-//        Pattern pattern = Pattern.compile("^[A-Z][a-z]{2,}"); //For Reference
-//        Matcher matcher = pattern.matcher("Drisya");  //For Reference
 
+    public static void main(String[] args) {
+
+        System.out.println("*** Welcome to User Registration Problem ***");
+/*
+ UC1: User need to enter Valid firstname
+*/
         boolean result1 = Pattern.matches("^[A-Z][a-z]{2,}","Drisya");
-        if (result1 == true)
+        if (result1)
             System.out.println("Valid First Name");
         else
             System.out.println("Invalid First Name");
+/*
+ UC2: User need to enter Valid Lastname
+*/
+        boolean result2 = Pattern.matches("^[A-Z][a-z]{2,}","Athul");
+        if (result2)
+            System.out.println("Valid Last Name");
+        else
+            System.out.println("Invalid Last Name");
     }
 }
